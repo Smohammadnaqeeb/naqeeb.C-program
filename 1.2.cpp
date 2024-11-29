038877f0e1d49e6b2c84cd85e6fdc6bae4b91ca5
Smohammadnaqeeb <s.md.naqeeb@gmail.com>
2024-11-29 16:14:35 +0530

    Add files via upload

---
+++ 1.2.cpp
@@ -0,0 +1,13 @@
+//C program to add two numbers
+#include<stdio.h>
+int main()
+{
+	int num1;
+	int num2;
+	int sum;
+	num1=10;
+	num2=20;
+	sum=num1+num2;
+	printf("Sum of two numbers=%d",sum);
+	return 0;
+}
---
+++ 1.3.cpp
@@ -0,0 +1,9 @@
+//C program to calculate area of circle
+#include<stdio.h>
+int main()
+{
+	float pi=3.14,area,r=2.2;
+	area=pi*r*r;
+	printf("Area of circle=%f",area);
+	return 0;
+}
